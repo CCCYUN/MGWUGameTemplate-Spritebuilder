d41f2b9737c423a3e240f1ed912fdccbd8a71ecc
CCCYUN <caoyun9006@gmail.com>
2015-02-22 22:20:28 -0800

    Add exit button
    Add exit button and create code connection with GameScene and exit
    button

---
+++ MGWUGameTemplate.spritebuilder/Source/GameScene.m
@@ -0,0 +1,27 @@
+//
+//  GameScene.m
+//  MGWUGameTemplate
+//
+//  Created by Yun on 2/22/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+-(void) didLoadFromCCB
+{
+    NSLog(@"GameScene created");
+}
+
+
+-(void) exitButtonPressed
+{
+    NSLog(@"Get me outa here");
+    CCScene* scene = [CCBReader loadAsScene:@"MainScene"];
+    CCTransition* transition = [CCTransition transitionFadeWithDuration:1.5];
+    [[CCDirector sharedDirector] presentScene:scene withTransition:transition];
+}
+
+@end
